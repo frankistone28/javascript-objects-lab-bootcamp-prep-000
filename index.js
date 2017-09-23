99f7104f089905acf69395c4468e364a3057cf03
Franklyn Thompson <frankistone28@outlook.com>
2017-09-23 11:11:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-    var obj = Object.assign({},object);
-    delete obj.key;
-    return obj;
+    var newObj = Object.assign({},object);
+    delete newObj.key;
+    return newObj;
 }
