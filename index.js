2612a71b832572b9e2e1cd83f3a0a7386dbd65b6
Franklyn Thompson <frankistone28@outlook.com>
2017-09-23 10:57:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
     //return object = Object.assign(object,{key : value});
-    var obj = object[key] = value;
+    Object.assign({},object,{[key] : value});
     return obj;
 }
